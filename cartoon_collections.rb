fa10ef069fc6afea0575ad892a4b6a0c1d32b454
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-28 19:24:00 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf_array)
+  dwarf_array.each_with_index do |dwarf, index|
+    puts "#{index + 1} #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map do |calls|
+    calls.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(short_words)
+  short_words.any? { |word| word.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  if snacks.include?("cheddar")
+    return "cheddar"
+  elsif snacks.include?("gouda")
+    return "gouda"
+  elsif snacks.include? ("camembert")
+    return "camembert"
+  end
 end
